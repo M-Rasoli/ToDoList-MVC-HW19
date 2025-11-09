be236f5ea36f651541e870dea1b03d6336676885
mohammad <mohammad@gmail.com>
2025-11-09 20:59:26 +0330

    Add project files.

---
+++ App.Domain.Core/TodoListAgg/Entities/TodoList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Domain.Core.TodoListAgg.Entities
+{
+    public class TodoList
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string DueDateShamsi { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
---
+++ App.EndPoints.Presentation.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace App.EndPoints.Presentation.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
