1eb568c42c74a71279fdf21d9b3f71a647915ec7
mohammad <mohammad@gmail.com>
2025-11-12 14:26:32 +0330

    add appservice project

--- App.Domain.Services/UserAgg/UserService.cs
+++ App.Domain.Services/UserAgg/UserService.cs
@@ -11,65 +11,24 @@ namespace App.Domain.Services.UserAgg
 {
     public class UserService(IUserRepository userRepository) : IUserService
     {
-        public Result<GetUserTasksDto> GetUserTasks(int userId)
+        public GetUserTasksDto GetUserTasks(int userId)
         {
-            var result = userRepository.GetUserTasks(userId);
-            if (result is not null)
-            {
-                return Result<GetUserTasksDto>.Success(message:"",result);
-            }
-            else
-            {
-                return Result<GetUserTasksDto>.Failure(message:"لیست کار های شما خالی است.");
-            }
+            return userRepository.GetUserTasks(userId);
         }
 
-        public Result<LoginUserDto> Login(string userName, string password)
+        public bool IsUserNameExist(string userName)
         {
-            var user = userRepository.Login(userName);
-            if (user is null)
-            {
-                return Result<LoginUserDto>.Failure(message:"نام کاربری یا رمز عبور اشتباه است.");
-            }
-
-            if (user.Password != password)
-            {
-                return Result<LoginUserDto>.Failure(message: "نام کاربری یا رمز عبور اشتباه است.");
-            }
-            else
-            {
-                return Result<LoginUserDto>.Success(message:"خوش آمدید",user);
-            }
-                
+            return userRepository.IsUserNameExist(userName);
         }
 
-        public Result<bool> Register(RegisterUserDto user)
+        public LoginUserDto Login(string userName)
         {
-            if (user.UserName == null ||
-                user.Password == null)
-            {
-                return Result<bool>.Failure(message:"فیلد های اجباری باید کامل شوند.");
-            }
-            if (userRepository.IsUserNameExist(user.UserName))
-            {
-                return Result<bool>.Failure(message:"نام کاربری قبلا استفاده شده است.");
-            }
+            return userRepository.Login(userName);
+        }
 
-            if (user.Password.Length < 3)
-            {
-                return Result<bool>.Failure(message:"رمز عبور حداقل باید شامل 3 کاراکتر باشد.");
-            }
-
-            var result = userRepository.Register(user);
-            if (result > 0)
-            {
-                return Result<bool>.Success(message:"ثبت نام با موفقیت انجام شد.");
-            }
-            else
-            {
-                return Result<bool>.Failure(message:"مشکلی پیش آمده لحظاتی بعد تلاش کنید.");
-            }
-
+        public int Register(RegisterUserDto user)
+        {
+            return userRepository.Register(user);
         }
     }
 }
